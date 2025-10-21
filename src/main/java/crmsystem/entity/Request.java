49e3682b73d2ae6bde10796c9418eff38c832f5c
Danchonaldo <danchonaldo@gmail.com>
2025-10-21 08:52:29 +0500

    .

--- src/main/java/crmsystem/entity/Request.java
+++ src/main/java/crmsystem/entity/Request.java
@@ -1,7 +1,11 @@
 package crmsystem.entity;
 
+import crmsystem.entity.Courses;
+import crmsystem.entity.Operators;
 import jakarta.persistence.*;
 import lombok.Data;
+
+import java.util.List;
 
 @Entity
 @Table(name = "requests")
@@ -12,18 +16,30 @@ public class Request {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "full_name", nullable = false)
-    private String fullName;
+    @Column(name = "user_name", nullable = false)
+    private String userName;
 
-    @Column(name = "course_name", nullable = false)
-    private String courseName;
+//    @Column(name = "course_name", nullable = false)
+//    private String courseName;
 
     @Column(name = "phone", nullable = false)
     private String phone;
 
-    @Column(columnDefinition = "TEXT")
-    private String comment;
+    @Column(name = "commentary", length = 1000)
+    private String commentary;
 
     @Column(nullable = false)
     private boolean handled = false;
+
+    @ManyToOne
+    @JoinColumn(name = "courses_id",nullable = false)
+    private Courses course;
+
+    @ManyToMany
+    @JoinTable(
+            name = "request_operators",
+            joinColumns = @JoinColumn(name = "request_id"),
+            inverseJoinColumns = @JoinColumn(name = "operator_id")
+    )
+    private List<Operators> operators;
 }
